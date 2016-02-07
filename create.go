c3716e93ce48c13c3f02e6a9f6192b4644794193
dbenoot <dbenoot@gmail.com>
2016-02-07 19:29:29 +0000

    update license and source comments

--- create.go
+++ create.go
@@ -1,16 +1,30 @@
-/* TODO
+//   Copyright 2016 The Stare Authors
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 
-- add metadata to new page: 
-    - date and time of creation
-    - availability in the menu
-    - order in the menu
-    - draft
-- add metadata to new gallery
-    - date and time of creation
-    - draft
-- create gallery folder
 
-*/
+// FUNCTIONS
+//
+//- create a page and add metadata to new page: 
+//    - date and time of creation
+//    - availability in the menu
+//    - order in the menu
+//    - name in menu
+//    - draft
+//- add metadata to new gallery
+//    - date and time of creation
+//    - draft
+//- create gallery folder
 
 package main
 
@@ -28,8 +42,6 @@ func create_page(pagename string) {
 
     fmt.Println("Creating page " + pagename)
     copyfile("." + string(filepath.Separator) + "templates" + string(filepath.Separator) + "page_template.html", "." + string(filepath.Separator) + "pages" + string(filepath.Separator) + pagename + ".html")
-
-//    page := string("pages/"+pagename+".html")
 
     now := time.Now().Format(time.RFC1123)
     prepend("status          : in_draft\n------------------------------------------------------------------------", "pages/"+pagename+".html")
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
-/* TODO
-
-- refine list command output
-- create render engine
-- create creation engine
-
-*/
+//   Copyright 2016 The Stare Authors
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 
 package main
 
@@ -16,8 +22,6 @@ import (
 )
 
 func main() {
-
-	
 
     renderCommand := flag.NewFlagSet("render", flag.ExitOnError)
 
@@ -131,5 +135,4 @@ func main() {
 		unpost_page(*pageUnpostFlag)
 	}
 
-    
 }
